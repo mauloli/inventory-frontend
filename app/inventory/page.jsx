9791b8e1c8a33ef7bb2db2c28b6dd2c5e0bfd414
Maulana Sholihin <maulanasholihinoli@gmail.com>
2023-12-28 23:39:48 +0700

    integrasi inventory

--- app/inventory/page.jsx
+++ app/inventory/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '@/components/layout/page';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
@@ -7,15 +7,41 @@ import './inventory.css'; // Import the external CSS file
 import Modals from '@/components/modal/page';
 import { BsArrowLeftSquareFill, BsPlusSquareFill } from 'react-icons/bs';
 import { useRouter } from 'next/navigation';
+import axios from '@/utils/axios';
 
 
 function Page() {
   const [open, setOpen] = useState(false);
+  const [result, setResult] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [skip, setSkip] = useState(0);
+
   const handleOpen = () => setOpen(true);
   const router = useRouter();
 
+  useEffect(() => {
+    getData();
+  }, []);
 
-  const dummyPages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  useEffect(() => {
+    getData();
+  }, [page]);
+
+  const getData = async () => {
+    try {
+      const resultt = await axios.get(`/inventory?$limit=${limit}&$skip=${skip}`);
+      const { data } = resultt;
+      setResult(data.data);
+      setTotal(data.total);
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    }
+  };
+
+
   return (
     <Layout selected={'inventory'}>
       <div style={{ width: '100%', backgroundColor: 'white' }}>
@@ -50,28 +76,36 @@ function Page() {
               </tr>
             </thead>
             <tbody>
-              {dummyPages.map((item, index) => (
-                <tr key={index}>
-                  <td>1</td>
-                  <td>tas</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                </tr>
-              ))}
+              {result.map((item, index) => {
+                const createdAtDate = new Date(item.device.created_at);
+                createdAtDate.toISOString();
+                const formattedDate = `${createdAtDate.getDate()} ${createdAtDate.toLocaleString('default', { month: 'short' })} ${createdAtDate.getFullYear()}`;
+                const status = item.status == 1 ? 'active' : item.status == 2 ? 'broken' : 'standby';
+                const arrayMac = item.mac_address.split('/');
+                const macAddress = arrayMac.length == 2 ? arrayMac[1] : arrayMac[0];
+                return (
+                  <tr key={index}>
+                    <td>{item.id}</td>
+                    <td>{item.hostname}</td>
+                    <td>{item.device.name}</td>
+                    <td>{item.device.brand}</td>
+                    <td>{item.device.type.type}</td>
+                    <td>{macAddress}</td>
+                    <td>{item.location.lokasi}</td>
+                    <td>{item.location.room}</td>
+                    <td>{item.ip_address}</td>
+                    <td>{formattedDate}</td>
+                    <td>{status}</td>
+                    <td>{'action'}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
         <div style={{ width: '100%', marginTop: '20px', display: 'flex', justifyContent: 'center' }}>
           <Stack spacing={2}>
-            <Pagination count={10} variant="outlined" shape="rounded" />
+            <Pagination count={Math.round(total / limit)} variant="outlined" shape="rounded" page={page} onChange={(e, v) => { setPage(v); setSkip((v - 1) * limit); }} />
           </Stack>
         </div>
       </div>
